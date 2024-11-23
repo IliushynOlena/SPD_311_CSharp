8123491d4b830e0a9ce26576ee9d6e2a836507da
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-11-23 16:01:01 +0200

    Inheritance, Indexers

---
+++ 08_Indexers/Program.cs
@@ -0,0 +1,178 @@
+﻿namespace _08_Indexers
+{
+    class Laptop
+    {
+        public string Model { get; set; }
+        public double Price { get; set; }
+        public override string ToString()
+        {
+            return $"Model : {Model}. Price : {Price}";
+        }
+    }
+    class Shop
+    {
+        Laptop[] laptops;
+        public Shop(int size)//3
+        {
+            laptops = new Laptop[size];
+        }
+        public int Length
+        {
+            get { return laptops.Length; }
+        }
+        public Laptop GetLaptop(int index)
+        {
+            if (index >= 0 && index < laptops.Length)
+            {
+                return laptops[index];
+            }
+            else { throw new IndexOutOfRangeException(); }
+        }
+        public void SetLaptop(int index, Laptop laptop)
+        {
+            if (index >= 0 && index < laptops.Length)
+            {
+                laptops[index] = laptop;
+            }           
+        }
+        public Laptop this[int index]//value
+        {
+            get
+            {
+                if (index >= 0 && index < laptops.Length)
+                {
+                    return laptops[index];
+                }
+                else { throw new IndexOutOfRangeException(); }
+            } 
+            set {
+
+                if (index >= 0 && index < laptops.Length)
+                {
+                    laptops[index] = value;
+                }
+            }
+        }
+        public Laptop this[string name]//value
+        {
+            get
+            {
+                foreach (var l in laptops)
+                {
+                    if( l.Model == name )
+                        return l;
+                }
+                return null;
+            }
+            //private set
+            //{
+            //    for (int i = 0; i < laptops.Length; i++)
+            //    {
+            //        if (laptops[i].Model == name)
+            //        {
+            //            laptops[i] = value;
+            //            break;
+            //        }
+            //    }
+            //}
+        }
+        public int FindByPrice(double price)
+        {
+            for (int i = 0; i < laptops.Length; i++)
+            {
+                if (laptops[i].Price == price)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public Laptop this[double price]
+        {
+            get
+            {
+                int index = FindByPrice(price);
+                if (index != -1)
+                {
+                    return laptops[index];
+                }
+                throw new Exception("Incorrect price");
+            }
+            set
+            {
+                int index = FindByPrice(price);
+                if (index != -1)
+                {
+                    this[index] = value;
+                }
+            }
+        }
+    }
+    public class MultArray
+    {
+        private int[,] array;
+        public int Rows { get; private set; }
+        public int Cols { get; private set; }
+        public MultArray(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            array = new int[rows, cols];
+        }
+        public int this[int r, int c]
+        {
+            get { return array[r, c]; }
+            set { array[r, c] = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MultArray multArray = new MultArray(2, 3);
+
+            //for (int i = 0; i < multArray.Rows; i++)
+            //{
+            //    for (int j = 0; j < multArray.Cols; j++)
+            //    {
+            //        multArray[i, j] = i + j;//indexator - set
+            //        Console.Write($"{multArray[i, j]} ");//indexator - get
+            //    }
+            //    Console.WriteLine();
+            //}
+            
+            Shop shop = new Shop(3);
+            try
+            {
+                //shop.SetLaptop(0, new Laptop() { Model = "HP", Price = 45000.99 });
+                //Console.WriteLine(shop.GetLaptop(10));
+                shop[0] = new Laptop() { Model = "HP", Price = 45000.99 };//set
+                shop[1] = new Laptop() { Model = "ASUS", Price = 32000.99 };//set
+                shop[2] = new Laptop() { Model = "DELL", Price = 12000.99 };//set
+                Console.WriteLine(shop["DELL"]);
+                //shop["DELL"] = new Laptop() {  Model = "Mac", Price=100000.33};
+                Console.WriteLine(shop["Mac"]);
+                //Console.WriteLine(shop[0]);//get
+                for (int i = 0; i < shop.Length + 1; i++)
+                {
+                    Console.WriteLine(shop[i]);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine();
+            shop[45000.99] = new Laptop() { Model = "Lenovo", Price = 15000 };
+            Console.WriteLine(shop[15000.00]);
+
+            for (int i = 0; i < shop.Length; i++)
+            {
+                Console.WriteLine(shop[i]);
+            }
+
+        }
+    }
+}
---
+++ 09_Inheritance/Program.cs
@@ -0,0 +1,160 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _09_Inheritance
+{
+    abstract class Person: Object
+    {
+        protected string name;
+        private readonly DateTime birthdate;
+        public Person()
+        {
+            name = "No name";
+            birthdate = new DateTime();   
+        }
+        public Person(string n, DateTime b)
+        {
+            name = n;
+            if (b > DateTime.Now)
+            {
+                birthdate = new DateTime();
+            }
+            else
+                birthdate = b;            
+        }
+        public abstract void DoWork();
+        public virtual void Print()
+        {
+            Console.WriteLine($"Name : {name}. DateBirthday : {birthdate.ToLongDateString()}");
+        }
+        public override string ToString()
+        {
+            return $"Name : {name}. DateBirthday : {birthdate.ToLongDateString()}";
+        }
+    }
+    //class Name : BaseClass, Interface1, Interface2, Interface3
+    class Worker: Person //public
+    {
+        private decimal salary;
+
+        public decimal Salary
+        {
+            get { return salary; }
+            set { this.salary = value >= 0 ? value : 0; }
+        }
+        public Worker(): base() 
+        {
+           Salary = 0;  
+        }
+        public Worker(string n, DateTime b, decimal s):base(n,b)
+        {
+            Salary = s;
+        }
+        public override void DoWork()
+        {
+            Console.WriteLine("Doing some work..........");
+        }
+        //new
+        public override void Print()
+        {
+           base.Print();
+            Console.WriteLine($"Salary : {Salary}");
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"Salary : {Salary}";
+        }
+    }
+    //sealed
+    sealed class Programmer : Worker
+    {
+        public int CodeLines { get; private set; }
+        public Programmer() : base()
+        {
+            CodeLines = 0;
+        }
+        public Programmer(string n, DateTime d, decimal s) : base(n, d, s)
+        {
+            CodeLines = 0;
+        }
+        public sealed override  void DoWork()
+        {
+            Console.WriteLine("Write C# code!!!!");
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Code lines {CodeLines}$");
+        }
+        public void WriteLines()
+        {
+            CodeLines++;
+        }
+    }
+    class TeamLead : Worker
+    {
+        public int CountProjects { get; set; }
+        //public override void DoWork()
+        //{
+        //    Console.WriteLine("Manage team project!");
+        //}
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person person = new Person("Ivan",new DateTime(2000,12,5));
+            //Console.WriteLine(person);
+
+            Worker worker = new Worker("Vova", new DateTime(2001,5,7), 20000);
+            worker.DoWork();
+            worker.Print();
+
+
+            Person[] persons = new Person[]
+            {
+                new Worker("Ivan",new DateTime(2000,12,5),1000),
+                worker,
+                new Worker("Oleg", new DateTime(1989,5,7), 15000),
+                new Programmer("Mukola",new DateTime(1999,5,7),35000)
+            };
+
+            foreach (var item in persons)
+            {
+                item.Print();
+                Console.WriteLine();
+            }
+
+            Programmer pr = null;
+            // 1 - use cast()
+            try
+            {
+                pr = (Programmer)persons[3];
+                pr.WriteLines();
+
+                pr.Print();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            // 2 - use as
+            pr = persons[2] as Programmer;
+            if (pr != null)
+            {
+                pr.WriteLines();
+
+                pr.Print();
+            }
+            else { Console.WriteLine("Null object!!!"); }
+            //3 - use is and as
+            if (persons[3] is Programmer)
+            {
+                pr = persons[3] as Programmer;
+                pr.WriteLines();
+
+                pr.Print();
+            }
+            else { Console.WriteLine("Null object!!!"); }
+        }
+    }
+}
